1aaeada3d20eeb9ee575d41bdb1f4e40611d6092
Yi126 <134844590+Yi126@users.noreply.github.com>
2024-11-15 22:08:39 +1030

    Update kd.py

--- CODE/kd.py
+++ CODE/kd.py
@@ -16,10 +16,10 @@ def distillation_loss(output, labels, teacher_output, T=10.0, alpha=0.5):
 
 def train_model(model, train_loader, criterion, optimizer, T=10.0, is_student=False, teacher_model=None, device=None):
     
-    model.to(device)  # 确保模型在正确的设备上
+    model.to(device)  
     for epoch in range(1000):
         if teacher_model:
-            teacher_model.to(device)  # 确保教师模型也在同一设备上
+            teacher_model.to(device) 
         model.train()
         for inputs, labels in train_loader:
             inputs, labels = inputs.to(device), labels.to(device)
@@ -42,12 +42,11 @@ def train_model(model, train_loader, criterion, optimizer, T=10.0, is_student=Fa
 results = []
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 for dataset_name in UNIVARIATE_DATASET_NAMES:
-    start_time = time.time()  # 记录训练开始时间
+    start_time = time.time()  
     train_loader, test_loader, train_shape, test_shape, ucr_nb_classes = ucr_loader(dataset_name, batch_size=128)
     
     teacher_model = Classifier_INCEPTION_Logits(input_shape=train_shape, nb_classes=ucr_nb_classes).to(device)
     student_model = Classifier_INCEPTION_Logits(input_shape=train_shape, nb_classes=ucr_nb_classes).to(device)
-    # 选择损失函数和优化器
     criterion = nn.CrossEntropyLoss()
     teacher_optimizer = optim.Adam(teacher_model.parameters())
     student_optimizer = optim.Adam(student_model.parameters())
@@ -73,15 +72,14 @@ for dataset_name in UNIVARIATE_DATASET_NAMES:
     #print(f"Epoch {epoch+1}, Loss: {loss.item()}")
 
     torch.save(student_model.state_dict(), f"/Project/Yi/defense/OUTPUT/kd/{dataset_name}_kd_model_weights.pth")
-    end_time = time.time()  # 记录训练结束时间
-    total_time = end_time - start_time  # 计算总训练时间
-    # 重新创建模型实例并加载权重
+    end_time = time.time()  
+    total_time = end_time - start_time 
 
     student_model = Classifier_INCEPTION(input_shape=train_shape, nb_classes=ucr_nb_classes).to(device)
     student_model.load_state_dict(torch.load(f"/Project/Yi/defense/OUTPUT/kd/{dataset_name}_kd_model_weights.pth"))
     # Testing student model
     student_model.eval()
-    dataset_results = []  # 初始化一个空列表来保存当前数据集的结果
+    dataset_results = []  
     for i in range(5):
         
         all_labels = []
@@ -91,24 +89,20 @@ for dataset_name in UNIVARIATE_DATASET_NAMES:
                 inputs, labels = inputs.to(device), labels.to(device)
                 outputs = student_model(inputs)
                 _, predicted = torch.max(outputs.data, 1)
-                # 将标签和预测添加到列表中
                 all_labels.extend(labels.cpu().numpy())
                 all_predictions.extend(predicted.cpu().numpy())
 
-        # 计算准确率和F1分数
         accuracy = accuracy_score(all_labels, all_predictions)
         f1 = f1_score(all_labels, all_predictions, average='weighted')
         dataset_results.append({
-            'Accuracy': accuracy * 100,  # 转换为百分比形式
+            'Accuracy': accuracy * 100,  
             'F1 Score': f1
         })
 
-    # 计算当前数据集的平均性能指标
     dataset_results_df = pd.DataFrame(dataset_results)
     mean_accuracy = dataset_results_df['Accuracy'].mean()
     mean_f1 = dataset_results_df['F1 Score'].mean()
 
-    # 将平均结果添加到总结果列表中
     results.append({
         'Dataset': dataset_name,
         'Average Accuracy': mean_accuracy,
@@ -116,13 +110,10 @@ for dataset_name in UNIVARIATE_DATASET_NAMES:
         'Total Training Time': total_time
     })
 
-# 将总结果列表转换为 DataFrame 并打印
 results_df = pd.DataFrame(results)
-# 定义 CSV 文件路径
+
 csv_file_path = '/Project/Yi/defense/OUTPUT/kd/resultskd.csv'
 
-# 将 DataFrame 保存到 CSV 文件
-# 如果文件不存在，就创建一个新文件并写入，如果文件已存在，就追加到文件中
 if os.path.exists(csv_file_path):
     results_df.to_csv(csv_file_path, mode='a', header=False, index=False)
 else:
